dbbaf69b3cd29779c0527add5c1063d2ea2ba777
Xinxing Jiang <jiangxinxing09@gmail.com>
2015-10-18 09:41:42 -0700

    Setup a blank Parse project.

---
+++ BlankParseProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BlankParseProject
+//
+//  Created by Xinxing Jiang on 10/14/15.
+//  Copyright © 2015 iosjjj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BlankParseProjectTests/BlankParseProjectTests.swift
@@ -0,0 +1,36 @@
+//
+//  BlankParseProjectTests.swift
+//  BlankParseProjectTests
+//
+//  Created by Xinxing Jiang on 10/14/15.
+//  Copyright © 2015 iosjjj. All rights reserved.
+//
+
+import XCTest
+@testable import BlankParseProject
+
+class BlankParseProjectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
